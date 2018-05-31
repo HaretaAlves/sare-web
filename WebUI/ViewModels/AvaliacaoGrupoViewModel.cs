8dc20df603c17b73e7a40718529d173e59bc40b9
Hareta Alves <hareta.alves@gmail.com>
2018-05-31 10:36:20 -0300

    implementação do backend referente ao consulta de avaliações

---
+++ WebUI/ViewModels/AvaliacaoGrupoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.ViewModels
+{
+    public class AvaliacaoGrupoViewModel
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+
+        public int GrupoID { get; set; }
+        public GrupoModel Grupo { get; set; }
+
+        public int UserID { get; set; }
+        public UsuarioModel User { get; set; }
+
+        public List<AvaliacaoGrupoModel> AvaliacoesGrupo { get; set; }
+
+    }
+}
